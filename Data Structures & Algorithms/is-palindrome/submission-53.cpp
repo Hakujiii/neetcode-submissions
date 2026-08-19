52a7bd645c9e0aa7235a1ecc7cc320091e85d71a
Hakujiii <maianhkhoa1092008@gmail.com>
2026-08-19 22:54:55 +0700

    Add: is-palindrome - submission-53

---
+++ Data Structures & Algorithms/is-palindrome/submission-53.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = 0;
+        int m = s.size() - 1;
+        while(n < m){
+        if(!isalnum(s[n])){n++; continue;};
+        if(!isalnum(s[m])){m--; continue;};
+        if(tolower(s[n]) != tolower(s[m])){
+            return false;
+        }
+        n++;
+        m--;
+        }
+        return true;
+    }
+};
