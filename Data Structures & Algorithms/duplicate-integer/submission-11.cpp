7758451aa217f76dc757c4021fcd8225a10c1ad1
Hakujiii <maianhkhoa1092008@gmail.com>
2026-09-02 22:12:28 +0700

    Add: duplicate-integer - submission-11

---
+++ Data Structures & Algorithms/duplicate-integer/submission-11.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> res;
+        for(int num : nums){
+          if(res.count(num)){
+              return true;
+          }
+          res.insert(num);
+        }
+        return false;
+    }
+};
